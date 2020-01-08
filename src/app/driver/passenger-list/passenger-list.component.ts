66a7756b981743a76a1e38802c82a9cc4fe2abba
Hashan <hashan.madusanka12m2@gmail.com>
2020-01-08 20:59:31 +0530

    passenger-list fixed

--- src/app/driver/passenger-list/passenger-list.component.ts
+++ src/app/driver/passenger-list/passenger-list.component.ts
@@ -5,15 +5,18 @@ import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { MatSnackBar } from '@angular/material';
-//import {  } from 'src/app/login/login.component';
+//import {} from 'src/app/login/login.component';
 
 
 export interface Passenger{ //Interface for Passenger
-  name: string;
-  email: string;
   address: string;
+  driverId: string;
+  email: string;
+  name: string;
   phone: string;
   pickupLocation: string;
+  tempDriverId:string;
+  passengerId: string;
 }
 
 export interface userCredentials { //Interface user Credentials
@@ -29,38 +32,50 @@ export interface userCredentials { //Interface user Credentials
   styleUrls: ['./passenger-list.component.scss']
 })
 export class PassengerListComponent implements OnInit {
-  private passengerDoc: AngularFirestoreCollection<Passenger>;
-  passengers: Observable<Passenger[]>;
+  private passengerDoc: AngularFirestoreCollection<Passenger>; //
+
+  passengersObservable: Observable<Passenger[]>; //creating an observable array of passengers
+  allPassengerList: Passenger[]; //full array of passengers
+  showingPassengerList: Passenger[] = [] as Passenger[] ; //display array
+
   private usersDoc: AngularFirestoreCollection<userCredentials>; 
-  
+
+  private passengerDriverId: string;
+
   constructor(
     private afs: AngularFirestore,
     private router: Router,
     private spinner: NgxSpinnerService,
-    private _snackBar: MatSnackBar,)
-    {  
-      this.spinner.show();
-      this.passengerDoc=this.afs.collection<Passenger>('users/user/passenger');
-
-      this.passengers=this.passengerDoc.snapshotChanges().pipe(
-        map(actions=>actions.map(a=>{
-          var data=a.payload.doc.data() as Passenger;
-          const id=a.payload.doc.id;
-          spinner.hide();
-          return {id,...data};
-        }))
-      )
-    }
+    private _snackBar: MatSnackBar)
+    {   }
 
   ngOnInit() 
   {
+    this.afs.collection('users/user/passenger').snapshotChanges().subscribe( array =>
+      {
+
+      this.allPassengerList = array.map( item =>{ //adding passenger's data and Id to one
+        return {
+          passengerId: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as Passenger ;
+      });
       
+      this.allPassengerList.forEach(element =>{ //filtering passengers for logged in driver
+        if(element.driverId == localStorage.getItem('driverId')){
+          this.showingPassengerList.push(element);
+        }
+      })
+
+    });
+
   }
 
-  openSnackBar(message: string, action: string){
+
+ /*openSnackBar(message: string, action: string){
     this._snackBar.open(message, action, {
       duration: 2000,
     });
-  }
+  }*/
 
 }
