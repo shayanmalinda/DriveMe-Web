a5d21d8f6d3a327e093a9f03f77e4f2c43acf9f8
Hashan <hashan.madusanka12m2@gmail.com>
2020-01-13 19:48:13 +0530

    driver-add_payment database connect

--- src/app/driver/payments/payments.component.ts
+++ src/app/driver/payments/payments.component.ts
@@ -1,4 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { MatSnackBar, MatStepper } from '@angular/material';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+
+export interface payment{ //Interface for payments
+  //name: string;
+  date: string;
+  driverId: string;
+  driverPaymentId: string;
+  isAccepted: boolean;
+  value: string;
+}
+
+export interface passenger{ //Interface for Passenger
+  address: string;
+  driverId: string;
+  email: string;
+  name: string;
+  phone: string;
+  pickupLocation: string;
+  tempDriverId:string;
+  passengerId: string;
+  isDeleted: boolean;
+}
 
 @Component({
   selector: 'app-payments',
@@ -7,9 +36,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsComponent implements OnInit {
 
-  constructor() { }
+  paymentForm = this.formBuilder.group({
+    passengerId: [''],
+    date: [''],
+    amount: ['']
+  });
 
-  ngOnInit() {
+  allPassengerList: passenger[]; //full array of passengers
+  showingPassengerList: passenger[] = [] as passenger[] ; //display array
+
+  paymentDate: string;
+  paymentDriverId: string;
+  paymentDriverPaymentId: string;
+  paymentIsAccepted: boolean;
+  paymentValue: string;
+  paymentId: string;
+  paymentPassengerId: string;
+  
+  waiting = false;
+  firstFormGroup: FormGroup;
+
+  payment: payment;
+
+  //temps
+  tempid: string;
+
+  //passenger Parts
+  passengerId : string;
+  passenger: Observable<passenger>;
+  passengerList: passenger []; 
+
+  private paymentDoc: AngularFirestoreDocument<payment>;
+  payments: Observable<payment>;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private afs: AngularFirestore,
+    private _snackBar: MatSnackBar,
+   // private afStorage: AngularFireStorage,
+    private route: ActivatedRoute,
+    //private cdRef: ChangeDetectorRef,
+    private spinner: NgxSpinnerService)
+   {
+      //this.spinner.show();
+   }
+
+  ngOnInit() 
+  {
+    
+    this.afs.collection('users/user/passenger').snapshotChanges().subscribe( array =>
+      {
+
+      this.allPassengerList = array.map( item =>{ //adding passenger's data and Id to one
+        return {
+          passengerId: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as passenger ;
+      });
+      
+      this.allPassengerList.forEach(element =>{ //filtering passengers for logged in driver
+        if(element.driverId == localStorage.getItem('driverId')){
+          this.showingPassengerList.push(element);
+        }
+      })
+      
+
+    });
+
+  }
+
+  registerpayment(formData)
+  {
+    this.waiting=true;
+    
+     this.tempid=this.afs.createId();//create a id for driverPaymentId
+
+    console.log('idinit',this.tempid);
+
+    this.payment={
+      date: formData.date,
+      value: formData.amount,
+      driverId: localStorage.getItem('driverId'),
+      driverPaymentId: this.tempid,//correct?
+      isAccepted: false
+    }
+
+    this.afs.collection('users/user/passenger/'+formData.passengerId+'/payments/').add(this.payment).then(_=>{
+      this.openSnackBar("Payment Details Added", "Done");
+      this.waiting=false;
+    })
+
+  }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
 }
+
