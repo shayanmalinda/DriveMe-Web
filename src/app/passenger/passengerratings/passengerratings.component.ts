18025f1232c1e24d44bc1dca71de0f0840dcf889
Charith <charithbiya@gmail.com>
2020-02-16 17:58:38 +0530

    passengerRtings

--- src/app/passenger/passengerratings/passengerratings.component.ts
+++ src/app/passenger/passengerratings/passengerratings.component.ts
@@ -1,4 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatSnackBar } from '@angular/material';
+
+export interface rating{//Interface Ratings
+  date: string;
+  stars: number;
+  rating: string;
+}
+
+export interface passenger{ //Interface for Passenger
+  address: string;
+  driverId: string;
+  email: string;
+  name: string;
+  phone: string;
+  pickupLocation: string;
+  tempDriverId:string;
+  isDeleted: boolean;
+}
+
+
 
 @Component({
   selector: 'app-passengerratings',
@@ -6,10 +31,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./passengerratings.component.scss']
 })
 export class PassengerratingsComponent implements OnInit {
+  //passenger
+  passengerObservable: Observable<passenger[]>;
+  passengerId : string;
+  passengerName : string;
 
-  constructor() { }
+  //ratings
+  allRatingsList: rating[];
 
-  ngOnInit() {
-  }
+  constructor(//constructor
+    private afs: AngularFirestore,
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private _snackBar: MatSnackBar,
+    )
+     { }
+
+  ngOnInit()
+ {
+    this.route.queryParams.subscribe(params => {
+    this.passengerId = params['passengerId']; 
+    this.passengerName=params['passengerName'];   
+  });
+
+  this.afs.collection('users/user/passenger/'+this.passengerId+'/ratings').snapshotChanges().subscribe(array =>
+    {
+      this.allRatingsList = array.map( item=>{
+        const data=item.payload.doc.data() as rating;
+        const id = item.payload.doc.id;
+        return {id,...data};
+      })
+     // console.log(this.allRatingsList);
+      
+    } );
+
+ }
 
 }
