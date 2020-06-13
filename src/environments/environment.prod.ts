dc9cc41d579b1ffc9c39f33563bc8d1ab04be3e9
shayanmalinda <shayanmalinda@gmail.com>
2020-06-13 20:57:42 +0530

    environment.prod.ts firebase variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAql9AnybLBLMIrE7A-nBl2WN46WarnJG0",
+    authDomain: "driveme-9bd2a.firebaseapp.com",
+    databaseURL: "https://driveme-9bd2a.firebaseio.com",
+    projectId: "driveme-9bd2a",
+    storageBucket: "driveme-9bd2a.appspot.com",
+    messagingSenderId: "161159282814",
+    appId: "1:161159282814:web:ce1f054faa7a366ff8182f",
+    measurementId: "G-32HS3WZDW0"
+  }
 };
